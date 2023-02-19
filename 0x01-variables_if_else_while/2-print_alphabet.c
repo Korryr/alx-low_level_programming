ac0310e72df7f6939bfd80e4d85d4ba25d27d320
Korryr <brayokorry17@gmail.com>
2023-02-19 15:45:59 -0800

    fff

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * main - a program that prints letters in lower case
+ *
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	char  letter;
+	for (letter = 'a'; letter <= 'z'; letter++)
+
+	putchar(letter);
+
+	putchar('\n');
+		return (0);
+}
