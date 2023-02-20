d071d1a03a136377174cde2d88f70678edb89c5a
Korryr <brayokorry17@gmail.com>
2023-02-19 17:42:52 -0800

    ddd

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+/**
+ * main - A program that writes numbers from 0 to 10
+ *
+ * Return: 0 always (success>
+ */
+
+int main(void)
+{
+	int num;
+	for (num = 0; num < 10; num++)
+		printf("%d", num);
+	return (0);
+}
