c99791e8877ab895e5aa91710a387059855200e8
Korryr <brayokorry17@gmail.com>
2023-02-23 10:42:13 -0800

    ddd

---
+++ 0x02-functions_nested_loops/kip.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - running while loops
+ *
+ * return: 0 always successs
+ */
+int main()
+{
+	int i;
+	int j;
+	while(i<10)
+	{
+		j=0;
+		while(j<3)
+		{
+
+			printf("2");
+		j++;
+	}
+
+
+
+
+
+
+
+
+		putchar('7');
+		i++;
+	}
+
+	return (0);
+}
