6ec23f77d9e31f3430a89f72f2007a38d07efa75
Korryr <brayokorry17@gmail.com>
2023-02-24 20:52:41 +0000

    longwaytogo

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * more_numbers - prints 1 - 14 ten times
+ *
+ * @i - variable
+ * @j - variable
+ */
+void more_numbers(void)
+
+int i;
+
+int j;
+
+for (i = 0; i <= 10; i++)
+
+{
+	for (j >= 'o' && j <= '14'; j++)
+
+	{
+		_putchar('j');
+
+	}
+	_putchar('\n');
+
+}
+
